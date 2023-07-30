83af68b0c2aab6c61de6ba16dc4d1ee31a45ec56
Prime Patel <primespatel@gmail.com>
2023-07-31 02:47:38 +0530

    updated social sites

--- theme.py
+++ theme.py
@@ -1,3 +1,3 @@
 with open("assets/css/main.css", 'r', encoding="utf-8") as css:
     with open("assets/css/style.css", 'w', encoding="utf-8") as file:
-        file.write(css.read().replace("C9F0D6", "F9F5F6"))
+        file.write(css.read().replace("C9F0D6", "F1F9F9"))
